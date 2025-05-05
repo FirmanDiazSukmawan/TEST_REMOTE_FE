cb95399b1d8a8694a1f05695f126b41449bee27c
xTats <firman670676@gmail.com>
2025-05-06 00:32:52 +0700

    feat: adding splashscreen and change logo apk

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import MainRouter from './app/route/MainRouter';
 import {View, StyleSheet} from 'react-native';
 import {enableScreens} from 'react-native-screens';
 import Toast from 'react-native-toast-message';
+import BootSplash from 'react-native-bootsplash';
 
 enableScreens();
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    setTimeout(async () => {
+      await BootSplash.hide();
+    }, 1500);
+  }, []);
+
   return (
     <NavigationContainer>
       <View style={styles.container}>
